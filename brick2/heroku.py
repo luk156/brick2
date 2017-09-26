edf0a14204cbba727e2d3070bd3ef03368021647
Matteo Lucchesi <matteo@luccalug.it>
2017-09-26 17:47:29 +0200

    fix

---
+++ brick2/heroku.py
@@ -0,0 +1,9 @@
+from brick2.settings import *
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+DEBUG = False
+
+ALLOWED_HOSTS = ['brick2.herokuapp.com']
